2dcededabaa2877dcbc82812db3a11e9a114b2d0
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-08-24 18:57:02 +0200

    Initial checkin.

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+try:
+    README = open('README.rst').read()
+except:
+    README = None
+
+try:
+    REQUIREMENTS = open('requirements.txt').read()
+except:
+    REQUIREMENTS = None
+
+setup(
+    name='spotify2piratebay',
+    version="0.1",
+    description='Download your Spotify music using the Pirate Bay',
+    long_description=README,
+    install_requires=REQUIREMENTS,
+    author='Mathijs de Bruin',
+    author_email='mathijs@visualspace.nl',
+    url='http://github.com/dokterbob/spotify2piratebay/',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=['Development Status :: 4 - Beta',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Utilities'],
+    # entry_points={
+    #     'console_scripts': [
+    #         'spotify2piratebay = spotify2piratebay.runner:main',
+    #     ],
+    # },
+)
